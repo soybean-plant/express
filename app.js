86e5828120b0315e3e0219ba2d674f163d07886f
soy bean <zwxec11@gmail.com>
2025-12-01 22:26:35 +0900

    Add mongodb access

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
+// var catRouter = require('./routes/cat');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+// app.use('/cat', catRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,8 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
