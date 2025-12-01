c0ef175a52d4b85cbdd5615931ce5a6353ed4ef7
soy bean <zwxec11@gmail.com>
2025-12-01 23:09:29 +0900

    add cat api

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
-// var catRouter = require('./routes/cat');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
-// app.use('/cat', catRouter);
+app.use('/cat', catRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ routes/cat.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');  // 事前に npm install request
+
+router.get('/', (req, res, next) => {
+  request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      res.json(data);  // 猫画像の情報をそのまま返す
+    } else {
+      next(error || new Error('Request failed'));
+    }
+  });
+});
+
+module.exports = router;
